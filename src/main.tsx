6bc329bdbd4338ee92818a4c99b7148045c3e499
ybysn <ybyxn7@gmail.com>
2026-05-26 06:03:59 +0800

    refactor: split application css by responsibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/app.css";
+import "./styles/variables.css";
+import "./styles/layout.css";
+import "./styles/editor.css";
+import "./styles/preview.css";
+import "./styles/components.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
